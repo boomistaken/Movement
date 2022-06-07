75b4a77b60151c3e736554305960f94a96afce71
boomistaken <102023579+boomistaken@users.noreply.github.com>
2022-06-07 15:14:47 +0200

    Example 108

--- Movement/Movement/Example108/AcceleratingBall.cs
+++ Movement/Movement/Example108/AcceleratingBall.cs
@@ -20,45 +20,65 @@ Methods:
 
 namespace Movement
 {
-	class AcceleratingBall : SpriteNode
-	{
-		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
+  class AcceleratingBall : SpriteNode
+  {
+    // your private fields here (add Velocity, Acceleration, and MaxSpeed)
+    private Vector2 Velocity;
+    private Vector2 Acceleration = new Vector2(0.06f, -0.03f);
+    private float MaxSpeed = 1600;
 
+    // constructor + call base constructor
+    public AcceleratingBall() : base("resources/ball.png")
+    {
+      Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 4);
+      Color = Color.RED;
+    }
 
-		// constructor + call base constructor
-		public AcceleratingBall() : base("resources/ball.png")
-		{
-			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 4);
-			Color = Color.RED;
-		}
+    // Update is called every frame
+    public override void Update(float deltaTime)
+    {
+      Move(deltaTime);
+      WrapEdges();
+      if (Velocity.Length() > MaxSpeed)
+      {
+        Acceleration = new Vector2(0, 0);
+      }
+    }
 
-		// Update is called every frame
-		public override void Update(float deltaTime)
-		{
-			Move(deltaTime);
-			BounceEdges();
-		}
+    // your own private methods
+    private void Move(float deltaTime)
+    {
+      // TODO implement
+      Position += Velocity * deltaTime;
+      Velocity += Acceleration;
+    }
 
-		// your own private methods
-		private void Move(float deltaTime)
-		{
-			// TODO implement
-			// Position += Velocity * deltaTime;
-		}
+    private void WrapEdges()
+    {
+      float scr_width = Settings.ScreenSize.X;
+      float scr_height = Settings.ScreenSize.Y;
+      float spr_width = TextureSize.X;
+      float spr_heigth = TextureSize.Y;
 
-		private void BounceEdges()
-		{
-			float scr_width = Settings.ScreenSize.X;
-			float scr_height = Settings.ScreenSize.Y;
-			float spr_width = TextureSize.X;
-			float spr_heigth = TextureSize.Y;
+      // TODO implement...
+      if (Position.X > scr_width)
+      {
+        Position = new Vector2(0, Position.Y);
+      }
+      else if (Position.X < 0)
+      {
+        Position = new Vector2(scr_width, Position.Y);
+      }
+      if (Position.Y > scr_height)
+      {
+        Position = new Vector2(Position.X, 0);
+      }
+      else if (Position.Y < 0)
+      {
+        Position = new Vector2(Position.X, scr_height);
+      }
 
-			// TODO implement...
-			if (Position.X > scr_width)
-			{
-				// ...
-			}
-		}
+    }
 
-	}
+  }
 }
