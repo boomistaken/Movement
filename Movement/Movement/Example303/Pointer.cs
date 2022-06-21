4b9926631e0d8d1a1f1567328d8288802d20bb95
boomistaken <102023579+boomistaken@users.noreply.github.com>
2022-06-21 14:04:25 +0200

    Example 303

--- Movement/Movement/Example303/Pointer.cs
+++ Movement/Movement/Example303/Pointer.cs
@@ -21,54 +21,54 @@ Methods:
 
 namespace Movement
 {
-	class Pointer : SpriteNode
-	{
-		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
+  class Pointer : SpriteNode
+  {
+    // your private fields here (add Velocity, Acceleration, and MaxSpeed)
+    private Vector2 Velocity;
+    private Vector2 Acceleration;
+    private float maxSpeed = 400;
 
+    // constructor + call base constructor
+    public Pointer() : base("resources/spaceship.png")
+    {
+      Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
+      Color = Color.YELLOW;
+    }
 
-		// constructor + call base constructor
-		public Pointer() : base("resources/spaceship.png")
-		{
-			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
-			Color = Color.YELLOW;
-		}
+    // Update is called every frame
+    public override void Update(float deltaTime)
+    {
+      PointToMouse(deltaTime);
+    }
 
-		// Update is called every frame
-		public override void Update(float deltaTime)
-		{
-			PointToMouse(deltaTime);
-			BounceEdges();
-		}
+    // your own private methods
+    private void PointToMouse(float deltaTime)
+    {
+      // Or just implement it in Example 110 Follower
 
-		// your own private methods
-		private void PointToMouse(float deltaTime)
-		{
-			// Or just implement it in Example 110 Follower
+      Vector2 mouse = Raylib.GetMousePosition();
+      // Console.WriteLine(mouse);
 
-			Vector2 mouse = Raylib.GetMousePosition();
-			// Console.WriteLine(mouse);
+      // Position = mouse; // incorrect!!
 
-			Position = mouse; // incorrect!!
+      // Rotation += deltaTime * Math.PI;  // incorrect!!
 
-			Rotation += deltaTime * Math.PI;  // incorrect!!
+      Position += Velocity * deltaTime;
+      Acceleration = mouse - Position;
+      Acceleration = Vector2.Normalize(Acceleration);
+      Velocity += Acceleration;
+      Rotation = MathF.Atan2(Velocity.Y, Velocity.X);
 
-			// TODO implement
-			// Position += Velocity * deltaTime;
-		}
+      switch (Velocity.Length())
+      {
+        case float x when x > maxSpeed:
+          Velocity = Vector2.Normalize(Velocity) * maxSpeed;
+          break;
+      }
 
-		private void BounceEdges()
-		{
-			float scr_width = Settings.ScreenSize.X;
-			float scr_height = Settings.ScreenSize.Y;
-			float spr_width = TextureSize.X;
-			float spr_heigth = TextureSize.Y;
+      // TODO implement
+      // Position += Velocity * deltaTime;
+    }
 
-			// TODO implement...
-			if (Position.X > scr_width)
-			{
-				// ...
-			}
-		}
-
-	}
+  }
 }
